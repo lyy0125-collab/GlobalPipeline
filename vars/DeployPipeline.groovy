bff372412225afbe203c804fd1deede15efbf2a4
lyy0125-collab <hsjsgsgsjsbk826@gmail.com>
2025-08-25 19:50:21 +0800

    Update DeployPipeline.groovy

--- vars/DeployPipeline.groovy
+++ vars/DeployPipeline.groovy
@@ -5,46 +5,50 @@ def call(Map config) {
             maven 'Maven-3.9.9'
             jdk 'jdk8'
         }
-        environment {
-            HOST_IP      = config.HOST_IP
-            USER         = config.USER
-            PORT         = config.PORT
-            PROJECT_NAME = config.PROJECT_NAME
-            GIT_PATH     = config.GIT_PATH
-            SOURCE_PATH  = config.SOURCE_PATH ?: ""
-            REMOTE_DIR   = config.REMOTE_DIR ?: "/opt/app/${config.PROJECT_NAME}/"
-            JAR_NAME     = config.JAR_NAME ?: "${config.PROJECT_NAME}.jar"
-            GIT_BRANCH   = config.GIT_BRANCH ?: 'main'
-        }
+
         stages {
+            stage('Init Envs') {
+                steps {
+                    script {
+                        env.HOST_IP      = config.HOST_IP
+                        env.USER         = config.USER
+                        env.PORT         = config.PORT
+                        env.PROJECT_NAME = config.PROJECT_NAME
+                        env.GIT_PATH     = config.GIT_PATH
+                        env.SOURCE_PATH  = config.SOURCE_PATH ?: ""
+                        env.REMOTE_DIR   = config.REMOTE_DIR ?: "/opt/app/${config.PROJECT_NAME}/"
+                        env.JAR_NAME     = config.JAR_NAME ?: "${config.PROJECT_NAME}.jar"
+                        env.GIT_BRANCH   = config.GIT_BRANCH ?: "main"
+                    }
+                }
+            }
+
             stage('Checkout') {
                 steps {
-                    git branch: GIT_BRANCH, url: GIT_PATH
+                    git branch: env.GIT_BRANCH, url: env.GIT_PATH
                 }
             }
 
             stage('Build') {
                 steps {
-                    sh "mvn clean package -f ./${SOURCE_PATH}pom.xml"
+                    sh "mvn clean package -f ./${env.SOURCE_PATH}pom.xml"
                 }
             }
 
             stage('Deploy') {
                 steps {
-                    script {
-                        sh """
-                        ssh ${USER}@${HOST_IP} 'mkdir -p ${REMOTE_DIR}'
-                        scp ${SOURCE_PATH}${PROJECT_NAME}/target/${JAR_NAME} ${USER}@${HOST_IP}:${REMOTE_DIR}
-                        ssh ${USER}@${HOST_IP} 'bash ${REMOTE_DIR}startup.sh'
-                        """
-                    }
+                    sh """
+                        ssh ${env.USER}@${env.HOST_IP} 'mkdir -p ${env.REMOTE_DIR}'
+                        scp ${env.SOURCE_PATH}${env.PROJECT_NAME}/target/${env.JAR_NAME} ${env.USER}@${env.HOST_IP}:${env.REMOTE_DIR}
+                        ssh ${env.USER}@${env.HOST_IP} 'bash ${env.REMOTE_DIR}startup.sh'
+                    """
                 }
             }
 
             stage('Verify') {
                 steps {
                     script {
-                        def url = "http://${HOST_IP}:${PORT}/actuator/health"
+                        def url = "http://${env.HOST_IP}:${env.PORT}/actuator/health"
                         def maxTime = 180
                         def interval = 5
                         def elapsed = 0
@@ -63,7 +67,7 @@ def call(Map config) {
                             lastResponse = sh(script: "cat /tmp/resp.txt || true", returnStdout: true).trim()
 
                             if (code == "200") {
-                                echo "✅ 服务 ${PROJECT_NAME} 健康检查成功 (HTTP 200)"
+                                echo "✅ 服务 ${env.PROJECT_NAME} 健康检查成功 (HTTP 200)"
                                 success = true
                                 break
                             } else {
@@ -78,7 +82,7 @@ def call(Map config) {
                             echo "------ 服务最后一次返回内容 ------"
                             echo lastResponse
                             echo "--------------------------------"
-                            error "服务 ${PROJECT_NAME} 启动失败"
+                            error "服务 ${env.PROJECT_NAME} 启动失败"
                         }
                     }
                 }
